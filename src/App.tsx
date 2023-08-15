f7374efc159a057796abe644d42135009a24ac4c
Roy.Liu <Roy.Liu@fanruan.com>
2023-08-15 17:34:39 +0800

    点赞按钮

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 export function App() {
+  const [isLiked, setIsLiked] = useState(false);
   return (
-    <div className="h-full w-full flex items-center justify-center ">点赞</div>
+    <div className="h-full w-full flex items-center justify-center flex-col">
+      <div
+        className={`like-button ${isLiked ? "liked" : ""}`}
+        onClick={() => setIsLiked(!isLiked)}
+      />
+      <span>点赞</span>
+    </div>
   );
 }
